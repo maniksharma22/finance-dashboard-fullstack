bc4c548be190b5f67177a0c1689c5a3d11e80d02
Manik Sharma <114878881+maniksharma22@users.noreply.github.com>
2026-04-06 07:59:40 +0530

    Update UserController.java

--- finance-backend/src/main/java/com/manik/financedashboard/controller/UserController.java
+++ finance-backend/src/main/java/com/manik/financedashboard/controller/UserController.java
@@ -4,46 +4,59 @@ import com.manik.financedashboard.model.Role;
 import com.manik.financedashboard.model.User;
 import com.manik.financedashboard.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
-// @CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 public class UserController {
 
     private final UserService userService;
 
+    @GetMapping("/profile")
+    public ResponseEntity<User> getUserProfile(@RequestParam String email) {
+        User user = userService.getUserByEmail(email);
+        return user != null ? ResponseEntity.ok(user) : ResponseEntity.notFound().build();
+    }
+
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @PostMapping
     public User createUser(@RequestBody User user) {
         return userService.createUser(user);
     }
 
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @GetMapping
     public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
 
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @PutMapping("/{id}")
     public User updateUser(@PathVariable Long id, @RequestBody User user) {
         return userService.updateUser(id, user);
     }
 
-    @PatchMapping("/{id}/status")
-    public User updateStatus(@PathVariable Long id, @RequestBody Map<String, Boolean> payload) {
-        boolean active = payload.get("enabled");
-        return userService.updateUserStatus(id, active);
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    @PatchMapping("/{id}/toggle-status")
+    public User toggleStatus(@PathVariable Long id) {
+        User user = userService.getUserById(id); 
+        boolean newStatus = !user.isActive(); 
+        return userService.updateUserStatus(id, newStatus);
     }
 
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @PatchMapping("/{id}/role")
     public User updateRole(@PathVariable Long id, @RequestBody Map<String, String> payload) {
         Role role = Role.valueOf(payload.get("role"));
         return userService.updateUserRole(id, role);
     }
 
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @DeleteMapping("/{id}")
     public void deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
